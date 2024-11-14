b281306d5e17e77cc8834259650365e428e2f118
oswald <omaldonado@fi.uba.ar>
2024-11-13 22:21:52 -0300

    agregada la plantilla del tp

--- decodificador.asm
+++ decodificador.asm
@@ -1,12 +1,41 @@
+; Colocar nombre y padron de los integrantes del grupo
 %include "macros.asm"
 
+global	main
 
-section .data
-    bienvenida dw "Hola a todos y bienvenidos a nuestro TP. Espero les gueste y se de su agrado. xoxoXOxoOoxX", 0
-section .bss
-section .text
-    global main
+section	.data
+	secuenciaBinariaA	db	0xC4, 0x94, 0x37, 0x95, 0x63, 0xA2, 0x1D, 0x3C 
+						db	0x86, 0xFC, 0x22, 0xA9, 0x3D, 0x7C, 0xA4, 0x51 
+						db	0x63, 0x7C, 0x29, 0x04, 0x93, 0xBB, 0x65, 0x18 
+	largoSecuenciaA		db	0x18 ; 24d
+
+	secuenciaImprmibleB db	"vhyAHZucgTUuznwTDciGQ8m4TuvUIyjU"
+	largoSecuenciaB		db	0x20 ; 32d
+
+	TablaConversion		db	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+	
+; Casos de prueba:
+; SecuenciaBinariaDePrueba db	0x73, 0x38, 0xE7, 0xF7, 0x34, 0x2C, 0x4F, 0x92
+;						   db	0x49, 0x55, 0xE5, 0x9F, 0x8E, 0xF2, 0x75, 0x5A 
+;						   db	0xD3, 0xC5, 0x53, 0x65, 0x68, 0x52, 0x78, 0x3F
+; SecuenciaImprimibleCodificada	db	"czjn9zQsT5JJVeWfjvJ1WtPFU2VoUng/"
+
+; SecuenciaImprimibleDePrueba db "Qy2A2dhEivizBySXb/09gX+tk/2ExnYb"
+; SecuenciaBinariaDecodificada	db	0x43, 0x2D, 0x80, 0xD9, 0xD8, 0x44, 0x8A, 0xF8 
+;								db	0xB3, 0x07, 0x24, 0x97, 0x6F, 0xFD, 0x3D, 0x81 
+;								db	0x7F, 0xAD, 0x93, 0xFD, 0x84, 0xC6, 0x76, 0x1B
+ 
+; Un codificador/decodificador online se puede encontrar en https://www.rapidtables.com/web/tools/base64-encode.html
+	
+section	.bss
+	secuenciaImprimibleA	resb	32
+	secuenciaBinariaB		resb	24
+	
+section	.text
 
     main:
-        print bienvenida
-        ret
+
+
+
+
+		ret
